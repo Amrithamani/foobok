b6fb7a056a6a36fae6f687f43b0cef87a7c39582
Amrithamani <amrimani91@gmail.com>
2014-12-15 13:45:10 -0500

    saving database and migration

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'foobok',
+            'username'  => 'root',
+            'password'  => '9Fol0EKiA0',
+        ),
+
+    ),
+
+);
